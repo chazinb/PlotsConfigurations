f43e389ced3da0a76744a87cb4b181d78fffb38d
Barbara <chazinb@ifca.unican.es>
2017-02-22 11:49:01 +0100

    22 Febrero 2017

--- Configurations/T2tt/FitAnalyzer.C
+++ Configurations/T2tt/FitAnalyzer.C
@@ -7,15 +7,16 @@
 *      between the data and mc background shapes in each of the Validation Regions (VR), namely, Validation Region after 
 *      asking a b jet (Tag) and Validation Region after vetoing a b jet (NoTag).                 
 *      
-*      Name of regions to perform the fit: VR1Tag, VR1NoTag, VR1 => mlfitVR1Tag.root, mlfitVR1NoTag.root, mlfitVR1.root 
+*      Name of regions to perform the fit: VR1Tag, VR1NoTag, VR1. Out files produced by the fit:  mlfitVR1Tag.root, mlfitVR1NoTag.root, mlfitVR1.root 
 *     
+*      The general adress and suffix used: 
+*
 *      Input fit file:   the corresponding mlfit.root ( "*./mlfitVR1Tag.root" || "*./mlfitVR1NoTag.root" || "*./mlfitVR1.root")
 *      Input shape file: "./Shapes/ShapesVR1.root"
 *      Output dir:       "./Datacards"
 *      Output suffix:    "Tag", "NoTag"
 *
-*      * == "./Datacards/ValidationRegions/" ### ¿QUÉ ES ÉSTO? CARPETA DONDE SE GUARDAN LOS OUTPUTS?
-*
+*      * == "./Datacards/ValidationRegions/" -> Directory where are keeped the fit file
 *
 * ================================================================================================================================
 */
@@ -45,7 +46,7 @@ TString DataSampleName = "01_PseudoDataSmeared"; // Data sample
 const int nBackgroundSamples = 7;
 TString BackgroundSampleName[nBackgroundSamples] = {"04_TTTo2L2Nu", "05_ST", "06_WW", "03_VZ", "07_Zjets", "09_TTW", "10_TTZ"}; // Background samples
 
-// init .....
+// init some stuff
 bool VRForChannel = false;
 TString FitDirectory = "shapes_fit_b";
 bool VR1FitSplitChannels = false;
@@ -53,8 +54,12 @@ TString shapeRoot = "./Shapes/Shapes_mStop-250to350_Sm300_Xm125.root"; // You ca
 
 /*-------------------------
  *        VR1Fit
- * -------------------------
- * ¿UNA DESCRIPCIÓN GENERAL DE LO QUE HACE ESTA FUNCIÓN?
+ *-------------------------
+ *   This function uses the data and background shapes to create the shape uncertainty histograms for each channel (ee,mm,em) and each cut.
+ *   The ratio between data/mc is used as SF to correct the mc shape. Because of not simetrical shape uncertanty is considered, here it is just calculated the systematic shape uncertainty up histogram. 
+ *   For the systematic shape uncertainty up histogram we are using the nominal one. 
+ *   
+ *   This shapes uncertainties are obtained only for TTbar + TW process in Tag region and only for WW process in NoTag region.
  * 
  *   @param TString FitFileName     => Input fit file  
  *   @param TString ShapeFileName   => Input shape file
@@ -91,10 +96,11 @@ void VR1Fit(TString FitFileName, TString ShapeFileName, TString OutputDirectory,
 
  /* Loop 1
   * ----------
-  * Looping over the three exclusive channels (ee,mm,em) to fill the DataHisto and BckHisto output histogram per channel by adding selectively the input h 
-  *    - Fill the DataHisto output histogram per exclusive channel (ee,mm,em) by adding the data histogram shape stored in the input shape file  
-  *    - Fill the BackHisto output histogram per exclusive channel (ee,mm,em) by adding selectively the best fit histogram shape of each background proccess stored in the input fit file
+  * Looping over the three exclusive channels (ee,mm,em) to fill the DataHisto and BckHisto output histogram per channel by adding in function of Tag, NoTag FitRegions: 
+  *    - In DataHisto the data histogram shape stored in the input shape file  
+  *    - In BackHisto the post fit histogram shape of each background proccess stored in the input fit file
   */
+
   // Special reference for data input shape file, namely, VR1_ + Tag, VR1_ + NoTag regions
   TString VRName  = "VR1_" + FitRegion;
 
@@ -112,7 +118,7 @@ void VR1Fit(TString FitFileName, TString ShapeFileName, TString OutputDirectory,
     
     for (int bkg = 0; bkg<nBackgroundSamples; bkg++) 
     {
-      // The combine mlfit.root post fit histograms do not keep the values of bin edges in x-axis, so, you need rebuild them to compare the shapes with data
+      // The combine mlfit.root post fit histograms do not keep the values of bin edges in x-axis, so, you need to rebuild them if you want to compare the shapes with data
       TString BackHistoName = FitDirectory + "/" + Bin + Channel[ch] + "/" + BackgroundSampleName[bkg];
       TH1F *FitBackHisto = (TH1F*) FitFile->Get(BackHistoName);
       TH1F *ThisBackHisto = new TH1F("Fit_" + BackgroundSampleName[bkg] + "_" + Channel[ch], "", nBins, LowEdgeHisto, HighEdgeHisto);
@@ -123,16 +129,16 @@ void VR1Fit(TString FitFileName, TString ShapeFileName, TString OutputDirectory,
 	ThisBackHisto->SetBinError(ib, FitBackHisto->GetBinError(ib));
 
       }
-      // Add selecting the backgrounds by FitRegion  ###No entiendo qué quiere decir esto...........
-      if (FitRegion=="Tag" && bkg<2) BkgHisto[ch]->Add(ThisBackHisto);// For the Tag FitRegion we add only "04_TTTo2L2Nu", "05_ST", "06_WW" background
-      else if (FitRegion=="NoTag" && bkg==2) BkgHisto[ch]->Add(ThisBackHisto); // For the NoTag FitRegion we add only "06_WW" background
+      // Add the backgrounds histograms selecting them in function of the FitRegion  
+      if (FitRegion=="Tag" && bkg<2) BkgHisto[ch]->Add(ThisBackHisto);// For the Tag FitRegion we add only "04_TTTo2L2Nu", "05_ST", "06_WW" backgrounds
+      else if (FitRegion=="NoTag" && bkg==2) BkgHisto[ch]->Add(ThisBackHisto); // For the NoTag FitRegion we add only "06_WW" backgrounds
       else DataHisto[ch]->Add(ThisBackHisto, -1.); // For the Tag FitRegion we subtract "03_VZ", "07_Zjets", "09_TTW", "10_TTZ"; For the NoTag FitRegion we subtract "04_TTTo2L2Nu", "05_ST", "03_VZ", "07_Zjets", "09_TTW", "10_TTZ"
 
      }
     
     // Fill the DataHisto and BckHisto output histogram in the inclusive channel (ll)
-    DataHisto[nChannels]->Add(DataHisto[ch]); 
-    BkgHisto[nChannels]->Add(BkgHisto[ch]);
+    DataHisto[nChannels] -> Add(DataHisto[ch]); 
+    BkgHisto[nChannels]  -> Add(BkgHisto[ch]);
 
   }
 
@@ -143,8 +149,8 @@ void VR1Fit(TString FitFileName, TString ShapeFileName, TString OutputDirectory,
   
   /* Loop 2
    * --------
-   * Save the DataHisto and BkgHisto output histograms. 
-   * Create a new one to store the ratio Data/MC in each bin. It is save in the same file and called "Ratio"
+   * Save the BkgHisto output histograms. 
+   * Create a new DataHisto[ch] storing the ratio Data/MC in each bin. It is saved and called "Ratio"
    */  
   for (int ch = 0; ch<nChannels+1; ch++) {
 
@@ -161,9 +167,12 @@ void VR1Fit(TString FitFileName, TString ShapeFileName, TString OutputDirectory,
  /* Loop 3
   * --------
   * Create the shape uncertainty histograms for each channel (ee,mm,em) and each cut 
+  *
   * Stop_02_VR1_Tag          Stop_02_SR1_Tag            Stop_02_SR2_Tag            Stop_02_SR3_Tag
   * Stop_02_VR1_NoTag        Stop_02_SR1_NoTag          Stop_02_SR2_NoTag          Stop_02_SR3_NoTag 
+  *
   */
+
   for (int ct = 0; ct<ncut; ct++)
   {
     TString SRName = scut[ct];
@@ -184,8 +193,8 @@ void VR1Fit(TString FitFileName, TString ShapeFileName, TString OutputDirectory,
       for (int tbg = 0; tbg<=2; tbg++)
       {
 
-	if (FitRegion=="Tag" && tbg==2) continue;  // use "04_TTTo2L2Nu", "05_ST" avoid "06_WW" background 
-	if (FitRegion=="NoTag" && tbg<2) continue; // use "06_WW" avoid "04_TTTo2L2Nu", "05_ST" background
+	if (FitRegion=="Tag" && tbg==2) continue;  // use "04_TTTo2L2Nu", "05_ST" avoid "06_WW" and the rest backgrounds 
+	if (FitRegion=="NoTag" && tbg<2) continue; // use "06_WW" avoid "04_TTTo2L2Nu", "05_ST" and the rest background
 	
 	TString HistoName = "histo_" + BackgroundSampleName[tbg];
 	TH1F *ThisHisto;
@@ -193,15 +202,15 @@ void VR1Fit(TString FitFileName, TString ShapeFileName, TString OutputDirectory,
 	else ThisHisto = (TH1F*) ShapeFile->Get(CutName + "/MT2ll/" + HistoName); // Take the corresponding background histograms of SR input shape file: shapeRoot
 	ThisHisto->Write();
 
-        //Create and save the systematic histograms 	
+        //Create and save the systematic shape uncertainty histograms 	
 	TString SystematicName = tbg<2 ? "_TopMT2llShape" : "_WWMT2llShape";
 	HistoName += SystematicName;
 	if (VRForChannel) HistoName += ("_" + Channel[ch]); 
-	ThisHisto->SetName(HistoName + "Down");
+	ThisHisto->SetName(HistoName + "Down"); // Actually is the nominal one
 	ThisHisto->Write();
 	if (VRForChannel) ThisHisto->Multiply(DataHisto[ch]); 
 	else ThisHisto->Multiply(DataHisto[nChannels]); 
-	ThisHisto->SetName(HistoName + "Up");
+	ThisHisto->SetName(HistoName + "Up"); // Systematic up 
 	ThisHisto->Write();
 
       }      
@@ -214,9 +223,10 @@ void VR1Fit(TString FitFileName, TString ShapeFileName, TString OutputDirectory,
 
 /* FitAnalyzer
  * -------------
- *  Ya que has descrito las anteriores funciones, estaría bien escribir una descripción a esta, aunque sea el 20% fácil :P
+ * 
+ *  This function call VR1Fit function through the parameter Action 
  *  
- *  @param TString Action ..................
+ *  @param TString Action 
  *  
  */
 
